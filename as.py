df18e89cf9a1aa3f4172e56828823104000b14f9
PARMESHVAR8 <parmeshvarbugaliya86@gmail.com>
2024-10-29 14:59:31 +0530

    first

---
+++ as.py
@@ -0,0 +1,22 @@
+
+from instabot import Bot
+
+
+
+bot = Bot()
+bot.login(username="par_mesh_var",password="P####")
+bot.follow("5_minutes_aur")
+
+#bot.upload_photo("", caption="")
+
+#bot.unfollow("")
+
+#bot.send_message("message",['user1','user2'])
+
+#followers = bot.get_user_followers("username")
+#for follower in followers:
+ #   print(bot.get_user_info(follower))
+
+#following = bot.get_user_following("username")
+#for Following in following:
+#   print(bot.get_user_info(Following))
